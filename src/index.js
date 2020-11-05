9c624792a8f991041c35fed085ec45adbbaaea26
Takuya Fukuju <chalkygames123@gmail.com>
2020-11-05 14:05:23 +0900

    dialog_controller.js を移動

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { Application } from 'stimulus'
 
-import DialogController from './dialog_controller'
+import DialogController from './controllers/dialog_controller'
 
 const application = Application.start()
 application.register('dialog', DialogController)
