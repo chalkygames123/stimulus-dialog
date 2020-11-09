25b2b75c648f3026a3c23150f8738b200ca728b3
Takuya Fukuju <chalkygames123@gmail.com>
2020-11-09 20:36:23 +0900

    リファクタリング (cleanup メソッドをインライン化)

--- src/controllers/dialog_controller.js
+++ src/controllers/dialog_controller.js
@@ -58,10 +58,12 @@ export default class extends Controller {
 
     this.element.scrollTop = 0
 
+    this.focusFirstTabbableDescendant()
+
     document.addEventListener('keydown', this.handleKeyDown)
 
     if (this.noTransition) {
-      this.cleanUp()
+      this.handleTransitionEnd()
     } else {
       this.element.addEventListener('transitionend', this.handleTransitionEnd)
     }
@@ -124,7 +126,7 @@ export default class extends Controller {
     document.removeEventListener('keydown', this.handleKeyDown)
 
     if (this.noTransition) {
-      this.cleanUp()
+      this.handleTransitionEnd()
     } else {
       this.element.addEventListener('transitionend', this.handleTransitionEnd)
     }
@@ -158,21 +160,6 @@ export default class extends Controller {
     })
   }
 
-  cleanUp() {
-    if (!this.noTransition) {
-      this.element.removeEventListener(
-        'transitionend',
-        this.handleTransitionEnd
-      )
-    }
-
-    if (this.isOpen) {
-      this.emit('shown')
-    } else {
-      this.emit('hidden')
-    }
-  }
-
   emit(type) {
     this.element.dispatchEvent(
       new CustomEvent(type, {
@@ -192,6 +179,17 @@ export default class extends Controller {
   handleTransitionEnd(e) {
     if (e && e.target !== this.element) return
 
-    this.cleanUp()
+    if (!this.noTransition) {
+      this.element.removeEventListener(
+        'transitionend',
+        this.handleTransitionEnd
+      )
+    }
+
+    if (this.isOpen) {
+      this.emit('shown')
+    } else {
+      this.emit('hidden')
+    }
   }
 }
