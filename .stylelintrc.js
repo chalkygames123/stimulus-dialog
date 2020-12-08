7f0140b3459485b2f2c213fa3fa41f2afa127ab1
Takuya Fukuju <chalkygames123@gmail.com>
2020-12-08 17:31:16 +0900

    @chalkygames123/stylelint-config に移行

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,45 +1,7 @@
 module.exports = {
-  extends: ['stylelint-config-twbs-bootstrap/css', 'stylelint-config-prettier'],
-  rules: {
-    'alpha-value-notation': 'number',
-    'at-rule-empty-line-before': [
-      'always',
-      {
-        except: ['blockless-after-same-name-blockless', 'first-nested'],
-        ignore: ['after-comment'],
-        ignoreAtRules: ['else'],
-      },
-    ],
-    'comment-empty-line-before': null,
-    'declaration-block-no-redundant-longhand-properties': true,
-    'declaration-empty-line-before': [
-      'always',
-      {
-        except: ['after-declaration', 'first-nested'],
-        ignore: ['after-comment'],
-      },
-    ],
-    'function-calc-no-invalid': true,
-    'hue-degree-notation': 'angle',
-    'no-empty-source': null,
-    'order/order': [
-      [
-        'custom-properties',
-        'declarations',
-        {
-          type: 'at-rule',
-          hasBlock: true,
-        },
-        'rules',
-      ],
-    ],
-    'rule-empty-line-before': [
-      'always',
-      {
-        except: ['first-nested'],
-        ignore: ['after-comment'],
-      },
-    ],
-    'selector-class-pattern': null,
-  },
+  extends: [
+    '@chalkygames123/stylelint-config/css',
+    'stylelint-config-prettier',
+    '@chalkygames123/stylelint-config/prettier',
+  ],
 }
