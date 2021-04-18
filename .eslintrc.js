1cbe5a80cfa89b38f39f86dae9d39357ea5a69ab
Takuya Fukuju <chalkygames123@gmail.com>
2021-04-18 18:23:50 +0900

    @chalkygames123/eslint-config に移行

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,4 @@
 module.exports = {
   root: true,
-  env: {
-    browser: true,
-  },
-  extends: ['airbnb-base', 'plugin:eslint-comments/recommended', 'prettier'],
-  rules: {
-    'import/no-default-export': ['error'],
-    'import/order': [
-      'error',
-      {
-        groups: [
-          'builtin',
-          'external',
-          'internal',
-          'unknown',
-          'parent',
-          'sibling',
-          'index',
-        ],
-        'newlines-between': 'always',
-        alphabetize: {
-          order: 'asc',
-        },
-      },
-    ],
-    'import/prefer-default-export': 'off',
-  },
+  extends: ['@chalkygames123', 'prettier'],
 }
