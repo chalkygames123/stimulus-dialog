98d2c1468ad4262f8458e2478243f089b258930b
Takuya Fukuju <chalkygames123@gmail.com>
2021-04-18 18:24:08 +0900

    src/index.js を微調整

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import { Application } from 'stimulus'
 import { DialogController } from './controllers'
 
 const application = Application.start()
+
 application.register('dialog', DialogController)
