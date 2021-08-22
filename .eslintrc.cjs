7210e85de1757ebd52e5fb15e82c98aaf4dffee9
Takuya Fukuju <chalkygames123@gmail.com>
2021-08-22 18:08:05 +0900

    ESM に移行

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,6 @@ module.exports = {
   root: true,
   extends: ['xo', 'plugin:unicorn/recommended', 'prettier'],
   rules: {
-    'unicorn/prefer-module': 'off',
     'unicorn/prevent-abbreviations': 'off',
   },
   overrides: [
